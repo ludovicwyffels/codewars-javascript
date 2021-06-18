7cea8918373982d72e30101dbf8debd20964f7f0
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2021-06-18 07:56:00 +0200

    feat(7-kyu): Airport itinerary

---
+++ 7-kyu/Airport itinerary/index.test.js
@@ -0,0 +1,22 @@
+const itinerary = require('.');
+test('2', () => {
+  expect(itinerary([{ in: 'TRN', out: 'FCO' }])).toBe('TRN-FCO');
+});
+
+test('6', () => {
+  expect(
+    itinerary([
+      { in: 'TRN', out: 'FCO' },
+      { in: 'CIA', out: 'JFK' },
+    ])
+  ).toBe('TRN-FCO-CIA-JFK');
+});
+
+test('10', () => {
+  expect(
+    itinerary([
+      { in: 'TRN', out: 'FCO' },
+      { in: 'FCO', out: 'JFK' },
+    ])
+  ).toBe('TRN-FCO-JFK');
+});
