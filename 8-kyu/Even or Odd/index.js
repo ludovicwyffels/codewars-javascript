8af6f5e13ad48319f317a3f9a4d598e848de9ec2
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2021-06-23 17:30:00 +0200

    feat(8-kyu): Even or Odd

---
+++ 8-kyu/Even or Odd/index.js
@@ -0,0 +1,29 @@
+/*
+  Title:
+    Even or Odd
+  Description:
+    Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+    
+    
+    
+  Examples:
+  Notes:
+  Kata Link:
+    https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+  Discuss Link:
+    https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/solutions
+*/
+// Long Solution
+/*
+function even_or_odd(number) {
+  return number % 2 === 0 ? 'Even' : 'Odd';
+}
+*/
+
+// Short Solution
+const even_or_odd = (number) => (number % 2 === 0 ? 'Even' : 'Odd');
+
+// Function Export
+module.exports = even_or_odd;
---
+++ 8-kyu/Even or Odd/index.test.js
@@ -0,0 +1,9 @@
+const even_or_odd = require('.');
+describe('Even or Odd', () => {
+  test('Test', () => {
+    expect(even_or_odd(2)).toBe('Even');
+    expect(even_or_odd(0)).toBe('Even');
+    expect(even_or_odd(7)).toBe('Odd');
+    expect(even_or_odd(1)).toBe('Odd');
+  });
+});
