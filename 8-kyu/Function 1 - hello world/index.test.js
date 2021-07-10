8e0073e3f7a6debbfca7f8768e879408c3db60ab
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2021-07-10 11:32:00 +0200

    feat(8-kyu): hello world

---
+++ 8-kyu/Function 1 - hello world/index.test.js
@@ -0,0 +1,10 @@
+const greet = require('.');
+
+describe('Testing function', function () {
+  it('Is it a function?', function () {
+    expect(typeof greet).toBe('function');
+  });
+  it('Correct return-value?', function () {
+    expect(greet()).toBe('hello world!');
+  });
+});
