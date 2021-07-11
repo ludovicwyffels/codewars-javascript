419d33119c6f42b78a6e263f72b5fff95553e9c2
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2021-07-11 10:25:00 +0200

    feat(8-kyu): Fake binary

---
+++ 8-kyu/Fake Binary/index.test.js
@@ -0,0 +1,10 @@
+
+const fakeBin = require('.');
+
+describe('Basic tests', () => {
+  it('Testing for fixed tests', () => {
+    expect(fakeBin('45385593107843568')).toBe('01011110001100111');
+    expect(fakeBin('509321967506747')).toBe('101000111101101');
+    expect(fakeBin('366058562030849490134388085')).toBe('011011110000101010000011011');
+  });
+});
