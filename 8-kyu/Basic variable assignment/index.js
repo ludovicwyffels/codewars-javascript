50013b2e031440a4a3a11820f461f4c37f4885bc
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2021-07-19 17:43:00 +0200

    feat(8-kyu): basic variable assignment

---
+++ 8-kyu/Basic variable assignment/index.js
@@ -0,0 +1,22 @@
+
+/*
+  Title:
+    Basic variable assignment
+  Description:
+    This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+  Examples:
+  Notes:
+  Kata Link:
+    https://www.codewars.com/kata/50ee6b0bdeab583673000025
+  Discuss Link:
+    https://www.codewars.com/kata/50ee6b0bdeab583673000025/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/50ee6b0bdeab583673000025/solutions
+*/
+// Long Solution
+const [a, b] = ['code', 'wa.rs'];
+const name = a + b;
+
+// Function Export
+module.exports = name;
---
+++ 8-kyu/Basic variable assignment/index.test.js
@@ -0,0 +1,8 @@
+
+const name = require('.');
+
+describe('Basic tests', () => {
+  it('Testing for fixed tests', () => {
+    expect(name).toBe('codewa.rs');
+  });
+});
