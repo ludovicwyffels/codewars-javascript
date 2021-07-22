663b736006ccf394302dd39522e85dfd15ee244f
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2021-07-22 18:38:00 +0200

    feat(6-kyu): who likes it

--- 6-kyu/Who likes it?/index.js
+++ 6-kyu/Who likes it?/index.js
@@ -1,9 +1,10 @@
+
 /*
   Title:
     Who likes it?
   Description:
     You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
-    Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
+    Implement the function likes which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
     likes [] -- must be "no one likes this"
     likes ["Peter"] -- must be "Peter likes this"
     likes ["Jacob", "Alex"] -- must be "Jacob and Alex like this"
@@ -64,6 +65,12 @@
     likes(["Max", "John", "Mark"], "Max, John and Mark like this").
     likes(["Alex", "Jacob", "Mark", "Max"], "Alex, Jacob and 2 others like this").
 
+    likes(&[]) // must be "no one likes this"
+    likes(&["Peter"]) // must be "Peter likes this"
+    likes(&["Jacob", "Alex"]) // must be "Jacob and Alex like this"
+    likes(&["Max", "John", "Mark"]) // must be "Max, John and Mark like this"
+    likes(&["Alex", "Jacob", "Mark", "Max"]) // must be "Alex, Jacob and 2 others like this"
+
     For 4 or more names, the number in and 2 others simply increases.
 
   Examples:
@@ -77,20 +84,31 @@
 */
 // Long Solution
 function likes (names) {
-  names = names || [];
   switch (names.length) {
     case 0:
       return 'no one likes this';
     case 1:
-      return names[0] + ' likes this';
+      return `${names[0]} likes this`;
     case 2:
-      return names[0] + ' and ' + names[1] + ' like this';
+      return `${names[0]} and ${names[1]} like this`;
     case 3:
-      return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
     default:
-      return (names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this');
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
   }
 }
+// Short Solution
+const likesShort = (names) => {
+  if (names.length === 0) return 'no one likes this';
+  if (names.length === 1) return `${names[0]} likes this`;
+  if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
+  if (names.length === 3) { return `${names[0]}, ${names[1]} and ${names[2]} like this`; };
+
+  return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+};
 
 // Function Export
-module.exports = likes;
+module.exports = {
+  likes,
+  likesShort
+};
--- 6-kyu/Who likes it?/index.test.js
+++ 6-kyu/Who likes it?/index.test.js
@@ -1,9 +1,19 @@
-const likes = require('.');
-test('who linkes it?', () => {
-  expect(likes([])).toBe('no one likes this');
-  expect(likes(['Peter'])).toBe('Peter likes this');
-  expect(likes(['Jacob', 'Alex'])).toBe('Jacob and Alex like this');
-  expect(likes(['Max', 'John', 'Mark'])).toBe('Max, John and Mark like this');
-  expect(likes(['Alex', 'Jacob', 'Mark', 'Max'])).toBe('Alex, Jacob and 2 others like this');
-  expect(likes()).toBe('no one likes this');
+
+const { likes, likesShort } = require('.');
+describe('example tests', function () {
+  it('should return correct text', function () {
+    expect(likes([])).toBe('no one likes this');
+    expect(likes(['Peter'])).toBe('Peter likes this');
+    expect(likes(['Jacob', 'Alex'])).toBe('Jacob and Alex like this');
+    expect(likes(['Max', 'John', 'Mark'])).toBe('Max, John and Mark like this');
+    expect(likes(['Alex', 'Jacob', 'Mark', 'Max'])).toBe('Alex, Jacob and 2 others like this');
+  });
+
+  it('short response should return correct text', function () {
+    expect(likesShort([])).toBe('no one likes this');
+    expect(likesShort(['Peter'])).toBe('Peter likes this');
+    expect(likesShort(['Jacob', 'Alex'])).toBe('Jacob and Alex like this');
+    expect(likesShort(['Max', 'John', 'Mark'])).toBe('Max, John and Mark like this');
+    expect(likesShort(['Alex', 'Jacob', 'Mark', 'Max'])).toBe('Alex, Jacob and 2 others like this');
+  });
 });
