9fad0a045e4ebc2e742205ef1da0d31a08d4e035
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2021-07-29 17:16:00 +0200

    feat(7-kyu): disemvowel trolls

---
+++ 7-kyu/Disemvowel Trolls/index.js
@@ -0,0 +1,27 @@
+
+/*
+  Title:
+    Disemvowel Trolls
+  Description:
+    Trolls are attacking your comment section!
+    A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+    Your task is to write a function that takes a string and return a new string with all vowels removed.
+    For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+    Note: for this kata y isn't considered a vowel.
+
+  Examples:
+  Notes:
+  Kata Link:
+    https://www.codewars.com/kata/52fba66badcd10859f00097e
+  Discuss Link:
+    https://www.codewars.com/kata/52fba66badcd10859f00097e/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/52fba66badcd10859f00097e/solutions
+*/
+// Long Solution
+function disemvowel (str) {
+  return str.match(/[^aeiou]/gi).join('');
+}
+
+// Function Export
+module.exports = disemvowel;
---
+++ 7-kyu/Disemvowel Trolls/index.test.js
@@ -0,0 +1,10 @@
+
+const disemvowel = require('.');
+
+describe('Basic tests', () => {
+  it('Testing for fixed tests', () => {
+    expect(disemvowel('This website is for losers LOL!')).toBe('Ths wbst s fr lsrs LL!');
+    expect(disemvowel("No offense but,\nYour writing is among the worst I've ever read")).toBe("N ffns bt,\nYr wrtng s mng th wrst 'v vr rd");
+    expect(disemvowel('What are you, a communist?')).toBe('Wht r y,  cmmnst?');
+  });
+});
