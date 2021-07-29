48158f58a1e22b24e59a0b463a47f6cfeb7f688e
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2021-07-29 18:27:00 +0200

    feat(7-kyu): hightest and lowest

---
+++ 7-kyu/Highest and Lowest/index.test.js
@@ -0,0 +1,8 @@
+
+const highAndLow = require('.');
+
+describe('Basic tests', () => {
+  it('Testing for fixed tests', () => {
+    expect(highAndLow('4 5 29 54 4 0 -214 542 -64 1 -3 6 -6')).toBe('542 -214');
+  });
+});
